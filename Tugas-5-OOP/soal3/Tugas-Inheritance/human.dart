a1a781333151952c91459de07e011d5dca0b3a3a
bulanmarseliawt <86941684+bulanmarseliawt@users.noreply.github.com>
2024-01-19 12:16:51 +0700

    Create human.dart

---
+++ Tugas-5-OOP/soal3/Tugas-Inheritance/human.dart
@@ -0,0 +1,10 @@
+// soal 3 (Inheritance)
+
+import 'titan.dart';
+
+class Human extends Titan {
+  Human(int powerPoint) : super(powerPoint);
+
+  String killAllTitans() => "Sasageyo ... Shinzo Sasageyo...";
+}
+
