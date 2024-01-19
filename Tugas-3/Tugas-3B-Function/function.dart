5842e786dfa710fc198200dc2c2a152e1819b7e8
bulanmarseliawt <86941684+bulanmarseliawt@users.noreply.github.com>
2024-01-19 10:22:46 +0700

    Create function.dart

---
+++ Tugas-3/Tugas-3B-Function/function.dart
@@ -0,0 +1,50 @@
+// 1. Function Teriak
+String teriak() {
+  return 'Halo Sanbers!';
+}
+
+void main() {
+  print(teriak()); // "Halo Sanbers!"
+}
+
+// 2. Function Kalikan
+int kalikan(int num1, int num2) {
+  return num1 * num2;
+}
+
+void main() {
+  var num1 = 12;
+  var num2 = 4;
+
+  var hasilKali = kalikan(num1, num2);
+  print(hasilKali); // 48
+}
+
+// 3. Function Introduce
+String introduce(String name, int age, String address, String hobby) {
+  return 'Nama saya $name, umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!';
+}
+
+void main() {
+  final String name = "Achmad";
+  final int age = 30;
+  final String address = "Jln. Malioboro, Yogyakarta";
+  final String hobby = "Gaming";
+
+  final perkenalan = introduce(name, age, address, hobby);
+  print(perkenalan); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogyakarta, dan saya punya hobby yaitu Gaming!"
+}
+
+// 4. Function Faktorial
+int faktorial(int n) {
+  if (n <= 0) {
+    return 1;
+  } else {
+    return n * faktorial(n - 1);
+  }
+}
+
+void main() {
+  var angka = 6;
+  print('$angka! = ${faktorial(angka)}');
+}
