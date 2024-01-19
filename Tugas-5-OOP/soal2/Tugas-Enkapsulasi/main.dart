4f2ec0154d8e9648408d87240d2ffa55bda8038e
bulanmarseliawt <86941684+bulanmarseliawt@users.noreply.github.com>
2024-01-19 12:16:37 +0700

    Create main.dart

---
+++ Tugas-5-OOP/soal2/Tugas-Enkapsulasi/main.dart
@@ -0,0 +1,9 @@
+// Soal 2 Enkapsulasi (Pembungkusan)
+
+import 'lingkaran.dart';
+
+void main() {
+  Lingkaran lingkaran = Lingkaran(-7.0);
+  print('Jari-jari lingkaran: ${lingkaran.jariJari}');
+  print('Luas lingkaran: ${lingkaran.luas}');
+}
